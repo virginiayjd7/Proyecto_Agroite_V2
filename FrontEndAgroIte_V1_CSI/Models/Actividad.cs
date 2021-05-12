4303456817d42b90c4822c73f5e3896c0b4d26c9
Virginia Aquino <37305214+virginiayjd7@users.noreply.github.com>
2021-05-12 14:44:21 -0500

    login

--- FrontEndAgroIte_V1_CSI/Models/Actividad.cs
+++ FrontEndAgroIte_V1_CSI/Models/Actividad.cs
@@ -5,6 +5,7 @@ namespace FrontEndAgroIte_V1_CSI.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Actividad")]
     public partial class Actividad
@@ -23,5 +24,25 @@ namespace FrontEndAgroIte_V1_CSI.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Usuario> Usuario { get; set; }
+
+        public List<Actividad> Listar()
+        {
+            var actividad = new List<Actividad>();
+
+            try
+            {
+                using (var db = new agroite())
+                {
+
+                    actividad = db.Actividad.ToList();
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return actividad;
+        }
     }
 }
--- FrontEndAgroIte_V1_CSI/Models/Usuario.cs
+++ FrontEndAgroIte_V1_CSI/Models/Usuario.cs
@@ -4,7 +4,11 @@ namespace FrontEndAgroIte_V1_CSI.Models
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
 
     [Table("Usuario")]
     public partial class Usuario
@@ -68,5 +72,80 @@ namespace FrontEndAgroIte_V1_CSI.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Pedido> Pedido { get; set; }
+        public List<Usuario> Listar()
+        {
+            var usuarios = new List<Usuario>();
+            try
+            {
+                using (var db = new agroite())
+                {
+                    usuarios = db.Usuario.Include("Actividad").ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return usuarios;
+        }
+        public void Guardar(HttpPostedFileBase imgfile1)
+        {
+            try
+            {
+                using (var db = new agroite())
+                {
+
+                    Stream fileStream = imgfile1.InputStream;
+                    System.IO.BinaryReader br = new System.IO.BinaryReader(fileStream);
+                    Byte[] bytes = br.ReadBytes((Int32)fileStream.Length);
+                    string base64 = Convert.ToBase64String(bytes, 0, bytes.Length);
+                    string imgbase64 = "data:image/png:base64," + base64;
+
+                    this.Foto_Perfil = bytes;
+
+                    db.Entry(this).State = EntityState.Added;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public ResponseModel Acceder(string Usuario, string Password)
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                //ModelBD es el ADO QUE HEMOS CREADO
+                using (var db = new agroite())
+                {
+                    //e10adc3949ba59abbe56e057f20f883e
+                    // Password = HashHelper.MD5(Password);//1234546
+
+                    var query = db.Usuario.Where(x => x.Alias == Usuario).Where(x => x.Contraseña == Password)
+                        .SingleOrDefault();
+
+                    if (query != null)
+                    {
+                        //     Session["idusario"] = query.IdUsuario.ToString();
+                        SessionHelper.AddUserToSession(query.IdUsuario.ToString());
+                        //  SessionHelper.AddUserToSession(IdUsuario.ToString());
+                        rm.SetResponse(true);
+                        rm.idusuario = query.IdUsuario.ToString();
+
+                    }
+                    else
+                    {
+                        rm.SetResponse(false, "Usuario y/o Password incorrectos ...");
+                    }
+
+                }
+            }
+
+            catch (Exception ex)
+            { throw; }
+            return rm;
+        }
     }
 }
