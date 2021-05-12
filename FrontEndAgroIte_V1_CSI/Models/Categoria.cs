1c6756c221fc69367e1c79984c088e2acec26fd2
Virginia Aquino <37305214+virginiayjd7@users.noreply.github.com>
2021-05-12 02:41:26 -0500

    maquetacion

---
+++ FrontEndAgroIte_V1_CSI/Models/Categoria.cs
@@ -0,0 +1,27 @@
+namespace FrontEndAgroIte_V1_CSI.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Categoria")]
+    public partial class Categoria
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Categoria()
+        {
+            Producto = new HashSet<Producto>();
+        }
+
+        [Key]
+        public int IdCategoria { get; set; }
+
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Producto> Producto { get; set; }
+    }
+}
