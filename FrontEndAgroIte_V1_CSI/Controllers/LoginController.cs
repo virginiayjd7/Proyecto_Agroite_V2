838b989243ef1fd91d79cd81412b078cca068bdd
rafcallata <rafcallataf@upt.pr>
2021-05-13 03:53:18 -0500

    Registro Login Comprador

--- FrontEndAgroIte_V1_CSI/Controllers/LoginController.cs
+++ FrontEndAgroIte_V1_CSI/Controllers/LoginController.cs
@@ -5,12 +5,13 @@ using System.Web;
 using System.Web.Mvc;
 using FrontEndAgroIte_V1_CSI.Filters;
 using FrontEndAgroIte_V1_CSI.Models;
+
 namespace FrontEndAgroIte_V1_CSI.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
-        private Usuario usuario = new Usuario();
+        private Usuario objusuario = new Usuario();
         private Actividad actividad = new Actividad();
         // GET: Login
         [NoLogin]
@@ -19,19 +20,17 @@ namespace FrontEndAgroIte_V1_CSI.Controllers
             return View();
         }
 
-        //public ActionResult Acceder(string usuario, string password)
-        //{
+        public ActionResult Acceder(string usuario, string password)
+        {
 
-        //    //var rm = usuario.Acceder(usuario, password);
-        //    //if (rm.response)
-        //    //{
-        //    //    rm.href = Url.Content("~/Usuario/Menu");
-        //    //    Session["idusuario"] = rm.idusuario;
-        //    //    string ud = Session["idusuario"].ToString();
-
-        //    //}
-        //    //return Redirect("~/Usuario/Menu");
-        //}
+            var rm = objusuario.Acceder(usuario, password);
+            if (rm.response)
+            {
+                Session["idusuario"] = rm.idusuario;
+                return Redirect("~/Home/Index");
+            }
+            return Redirect("~/Login/Index");
+        }
 
         public ActionResult LogOut()
         {
@@ -47,26 +46,29 @@ namespace FrontEndAgroIte_V1_CSI.Controllers
         {
             return View();
         }
-       
-        //public ActionResult Registrarse()
-        //{
-        //    ViewBag.Tipo3 = actividad.Listar();
-        //    return View(new Usuario());
-        //}
-        //[HttpPost]
-        //public ActionResult Guardar(Usuario model, HttpPostedFileBase imgfile1)
-        //{
-        //    if (imgfile1.ContentLength > 0)
-        //    {
-        //        ModelState.Remove("Foto_Perfil");
-        //        if (ModelState.IsValid)
-        //        {
-        //            model.Guardar(imgfile1);
-        //            return Redirect("~/Login/Index");
-        //        }
 
-        //    }
-        //    return Redirect("~/Usuario/AgregarEditar");
-        //}
+        public ActionResult Registrarse()
+        {
+            ViewBag.Tipo3 = actividad.Listar();
+            return View(new Usuario());
+        }
+        [HttpPost]
+        public ActionResult Guardar(Usuario model, HttpPostedFileBase imgfile1)
+        {
+            if (imgfile1 == null)
+            {
+                return Redirect("~/Login/LogOut");
+            }
+            if (imgfile1.ContentLength > 0)
+            {
+                ModelState.Remove("Foto_Perfil");
+                if (ModelState.IsValid)
+                {
+                    model.Guardar(imgfile1);
+                    return Redirect("~/Login/Index");
+                }
+            }
+            return Redirect("~/Login/LogOut");
+        }
     }
 }
--- FrontEndAgroIte_V1_CSI/Models/ResponseModel.cs
+++ FrontEndAgroIte_V1_CSI/Models/ResponseModel.cs
@@ -13,6 +13,7 @@ namespace FrontEndAgroIte_V1_CSI.Models
         public string href { get; set; }
         public string function { get; set; }
         public string idusuario { get; set; }
+        public string actividad { get; set; }
 
         public ResponseModel()
         {
