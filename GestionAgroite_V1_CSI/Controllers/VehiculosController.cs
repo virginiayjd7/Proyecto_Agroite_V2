a5de79c9098c8f56875d96fb905059e39e795736
Virginia Aquino <37305214+virginiayjd7@users.noreply.github.com>
2021-05-14 18:04:29 -0500

    gestion

---
+++ GestionAgroite_V1_CSI/Controllers/VehiculosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionAgroite_V1_CSI.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        // GET: Vehiculos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GestionAgroite_V1_CSI/Models/Venta.cs
@@ -0,0 +1,45 @@
+namespace GestionAgroite_V1_CSI.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Venta")]
+    public partial class Venta
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Venta()
+        {
+            DetalleVenta = new HashSet<DetalleVenta>();
+            Entrega = new HashSet<Entrega>();
+        }
+
+        [Key]
+        public int IdVenta { get; set; }
+
+        [StringLength(50)]
+        public string Fecha { get; set; }
+
+        public int? Num_Serie { get; set; }
+
+        public int? IdPedido { get; set; }
+
+        public decimal? Total { get; set; }
+
+        public decimal? IGV { get; set; }
+
+        public int? IdTransportador { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<DetalleVenta> DetalleVenta { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Entrega> Entrega { get; set; }
+
+        public virtual Pedido Pedido { get; set; }
+
+        public virtual Transportador Transportador { get; set; }
+    }
+}
