687b7b7f9b8b4e3b26afbc920f41fbd8a17d92f6
Virginia Aquino <37305214+virginiayjd7@users.noreply.github.com>
2021-05-16 00:17:39 -0500

    carrito

--- FrontEndAgroIte_V1_CSI/Controllers/HomeController.cs
+++ FrontEndAgroIte_V1_CSI/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace FrontEndAgroIte_V1_CSI.Controllers
         // GET: Home
         public Usuario usuario = new Usuario();
         public Producto producto = new Producto();
+
+        public static List<Carrito> lstCarrito = new List<Carrito>();
         public ActionResult Index(string criterio)
         {
             if (criterio == null || criterio == "")
@@ -41,7 +43,51 @@ namespace FrontEndAgroIte_V1_CSI.Controllers
         }
         public ActionResult detallecarrito()
         {
-            return View();
+            return View(lstCarrito);
+        }
+
+        [ChildActionOnly]
+        public ActionResult _Carrito()
+        {
+            List<Carrito> lista = lstCarrito;
+         
+
+            return PartialView(lista);            
+        }
+      
+        public ActionResult AgregarCArritoMomentaneo(Carrito o)
+        {
+            Carrito ca = new Carrito();
+            Boolean existe = false;          
+            foreach (var item in lstCarrito)
+            {
+                if (item.id_producto == o.id_producto)                
+                {
+                    existe = true;                    
+                    break;
+                }
+            }
+
+            if (existe)
+            {
+                int index = lstCarrito.FindIndex(c => c.id_producto == o.id_producto);
+                int can = lstCarrito[index].cantidad;
+                decimal sub = lstCarrito[index].subtotal;
+                lstCarrito[index].cantidad = can+ o.cantidad;
+                lstCarrito[index].subtotal = sub + o.subtotal;
+            }
+            else
+            {
+                ca.AgrrgarCrrito(o);
+            }
+            return Json("Agregado", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Eliminar(Carrito o)
+        {
+              
+            lstCarrito = lstCarrito.Where(c => c.id_producto != o.id_producto).ToList();
+            return Json("eliminado", JsonRequestBehavior.AllowGet);
         }
     }
 }
