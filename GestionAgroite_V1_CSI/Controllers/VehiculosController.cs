2d65580d5b78eeaf29c64ae4613876ffb7708e46
rafcallata <rafcallataf@upt.pr>
2021-05-17 02:18:21 -0500

    Crud Vehiculos Gestion

--- GestionAgroite_V1_CSI/Controllers/VehiculosController.cs
+++ GestionAgroite_V1_CSI/Controllers/VehiculosController.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GestionAgroite_V1_CSI.Models;
 
 namespace GestionAgroite_V1_CSI.Controllers
 {
     public class VehiculosController : Controller
     {
+        Vehiculos objVehiculo = new Vehiculos();
         // GET: Vehiculos
-        public ActionResult Index()
+        public ActionResult Index(string criterio)
         {
-            return View();
+            if (criterio == null || criterio == "")
+            {
+                return View(objVehiculo.Listar());
+            }
+            else
+            {
+                return View(objVehiculo.Buscar(criterio));
+            }
+        }
+        public ActionResult AgregarEditar(int id = 0)
+        {
+            return View(id == 0 ? new Vehiculos() : objVehiculo.Obtener(id));
+        }
+
+        public ActionResult Guardar(Vehiculos model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Guardar();
+                return Redirect("~/Vehiculos");
+            }
+            else
+            {
+                return View("~/Vehiculos/AgregarEditar", model);
+            }
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            objVehiculo.IdVehiculo = id;
+            objVehiculo.Eliminar();
+            return Redirect("~/Vehiculos");
+        }
+        public ActionResult Visualizar(int id)
+        {
+            return View(objVehiculo.Obtener(id));
         }
     }
 }
